b5448614bf2a7883081b2c84966446574513a0ab
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-04-28 13:09:20 -0500

    Add files via upload

---
+++ datosGifs.js
@@ -0,0 +1,17 @@
+const obtenerGif= async () => {
+    const gifsContainers = document.getElementById('list-gifs');
+    const gifTemplate = document.getElementById('gif'); 
+
+    const request = await fetch("https://api.giphy.com/v1/gifs/search?api_key=lerx9FDWD5PH78M207Cj95ac4KTt8G3v&q=Goku-ssj1"); 
+
+    const respuesta = await request.json();
+
+    respuesta.data.slice(0, 8).forEach(gif => {
+        const newGifCard = gifTemplate.cloneNode(true);
+        const img = newGifCard.querySelector('img');
+        img.src = gif.images.original.url; 
+        gifsContainers.appendChild(newGifCard);
+    });
+    gifTemplate.remove();
+}
+obtenerGif();
